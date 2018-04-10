339c632b0dce5b2bd235f71af56d06f96f36b5f1
Oscar Lima <olima_84@yahoo.com>
2018-04-10 14:21:24 +0100

    add maps and datasets to repo

---
+++ data/get_datasets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get ISR cobot datasets from prof. Rodrigo Ventura website
+
+wget users.isr.ist.utl.pt/~yoda/tar/dataset_2013-10-09-15-10-03.bag
+wget users.isr.ist.utl.pt/~yoda/tar/dataset_2013-10-09-16-15-01.bag
+wget users.isr.ist.utl.pt/~yoda/tar/dataset_2013-10-09-16-41-06.bag
+wget users.isr.ist.utl.pt/~yoda/tar/dataset_2013-10-09-16-47-15.bag
+wget users.isr.ist.utl.pt/~yoda/tar/dataset_2013-10-09-17-58-51.bag
